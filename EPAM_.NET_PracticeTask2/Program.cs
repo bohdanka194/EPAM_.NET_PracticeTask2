37ae8bb91e6ec1ad16088ccbefb11bd85de46b04
bohdanka194 <bohdanka194@gmail.com>
2017-03-15 16:50:18 +0200

    Add project files.

---
+++ EPAM_.NET_PracticeTask2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPAM_.NET_PracticeTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating rectangle1...");
+            Rectangle rectangle1 = new Rectangle(1, 5, 4, 3);
+            Console.WriteLine("rectangle1:\n" + rectangle1.ToString());
+            Thread.Sleep(3000);
+
+            Console.WriteLine("\nCreating rectangle2...");
+            Rectangle rectangle2 = new Rectangle(0, 100, 10, 4);
+            Console.WriteLine("rectangle2:\n" + rectangle2.ToString());
+            Thread.Sleep(3000);
+
+            Console.WriteLine("\nCreating rectIntersect = intersection of rectangle1 and rectangle2...");
+            Rectangle rectIntersect = rectangle1.IntersectionWithRectangle(rectangle2);
+            Console.WriteLine("rectIntersect: \n" + rectIntersect.ToString());
+            Thread.Sleep(3000);
+
+            Rectangle HugeRect = Rectangle.FindRectangleThatContains2Rectangles(rectangle1, rectangle2);
+            Console.WriteLine("\nrectangle that contains rectangle1 and rectangle2 : \n" + HugeRect.ToString());
+            Thread.Sleep(3000);
+
+            Console.WriteLine("\nMove rectangle1 to -4(line x) and 0(line y)");
+            rectangle1.MoveRectangle(-4, 0);
+            Console.WriteLine("rectangle1:\n" + rectangle1.ToString());
+            Thread.Sleep(3000);
+
+            Rectangle HugeRect2 = Rectangle.FindRectangleThatContains2Rectangles(rectangle1, rectangle2);
+            Console.WriteLine("\nrectangle that contains rectangle1 and rectangle2 : \n" + HugeRect2.ToString());
+            Thread.Sleep(3000);
+
+            Console.WriteLine("\nResizing rectangle1 by coefficient = 3...");           
+            rectangle1.ReSizeRectangle(3);
+            Console.WriteLine("rectangle1:\n" + rectangle1.ToString());
+            Thread.Sleep(1500);
+
+            Console.WriteLine("\nSuccessfully completed the program!");
+            Console.Read();
+        }
+    }
+}
---
+++ EPAM_.NET_PracticeTask2/Rectangle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_.NET_PracticeTask2
+{
+    class Rectangle
+    {
+        private double xUpLeft;
+        private double yUpLeft;
+        private double xDownRight;
+        private double yDownRight;
+
+        public Rectangle(double xUpLeft, double yUpLeft, double xDownRight, double yDownRight)
+        {
+            if (xUpLeft > xDownRight || yUpLeft < yDownRight)
+            {
+                throw new Exception("Incorrect point of rectangle"); 
+            }
+
+            this.xUpLeft = xUpLeft;
+            this.yUpLeft = yUpLeft;
+            this.xDownRight = xDownRight;
+            this.yDownRight = yDownRight;
+        }
+
+        public void ReSizeRectangle(double coeff)
+        {
+            double xLength = this.xDownRight - this.xUpLeft;
+            double yLength = this.yUpLeft - this.yDownRight;
+
+            this.xDownRight = this.xUpLeft + coeff * xLength ;
+            this.yDownRight = this.yUpLeft - coeff * yLength;
+        }
+
+        public void MoveRectangle(double xMove,double yMove)
+        {
+            this.xUpLeft += xMove;
+            this.yUpLeft += yMove;
+            this.xDownRight += xMove;
+            this.yDownRight += yMove;
+        }
+
+        public static Rectangle FindRectangleThatContains2Rectangles(Rectangle rect1,Rectangle rect2)
+        {
+            Rectangle resRectangle;
+            double xUpLeftRes = rect1.xUpLeft < rect2.xUpLeft ? rect1.xUpLeft : rect2.xUpLeft;
+            double xDownRigthRes = rect1.xDownRight > rect2.xDownRight ? rect1.xDownRight : rect2.xDownRight;
+            double yUpLeftRes = rect1.yUpLeft > rect2.yUpLeft ? rect1.yUpLeft : rect2.yUpLeft;
+            double yDownRightRes = rect1.yDownRight < rect2.yDownRight ? rect1.yDownRight : rect2.yDownRight; 
+
+            resRectangle = new Rectangle(xUpLeftRes, yUpLeftRes, xDownRigthRes, yDownRightRes);
+            return resRectangle;
+        }
+
+        public Rectangle IntersectionWithRectangle(Rectangle rectToIntersect)
+        {
+            Rectangle resRectangle;
+            if (this.xDownRight < rectToIntersect.xUpLeft ||
+                this.xUpLeft > rectToIntersect.xDownRight ||
+                this.yUpLeft < rectToIntersect.yDownRight ||
+                this.yDownRight > rectToIntersect.yUpLeft)
+            {
+                return resRectangle = null;
+            }
+            double _xDownRight = this.xDownRight > rectToIntersect.xDownRight ? rectToIntersect.xDownRight : this.xDownRight;
+            double _xUpLeft = this.xUpLeft > rectToIntersect.xUpLeft ? this.xUpLeft : rectToIntersect.xUpLeft;
+            double _yDownRight = this.yDownRight > rectToIntersect.yDownRight ? this.yDownRight : rectToIntersect.yDownRight;
+            double _yUpLeft = this.yUpLeft > rectToIntersect.yUpLeft ? rectToIntersect.yUpLeft : this.yUpLeft;
+
+            resRectangle = new Rectangle(_xUpLeft,_yUpLeft,_xDownRight, _yDownRight);    
+            return resRectangle;
+        }
+
+        public override string ToString()
+        {
+            return "(" + Math.Round(xUpLeft, 2) + ";" + Math.Round(yUpLeft, 2) + ")\t(" + Math.Round(xDownRight, 2) + ";" + Math.Round(yUpLeft, 2) + ")\n(" +
+                Math.Round(xUpLeft, 2) + ";" + Math.Round(yDownRight, 2) + ")\t(" + Math.Round(xDownRight, 2) + ";" + Math.Round(yDownRight, 2) + ")";
+        }
+
+    }
+}
